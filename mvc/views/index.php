4f8904103f0eb7b3155c28ad7e765a440b9edec9
bruno bento <66705552+BDBento@users.noreply.github.com>
2023-04-05 21:57:55 -0400

    nome dos alunos

--- mvc/views/index.php
+++ mvc/views/index.php
@@ -1,2 +1,8 @@
 <h1>Site de Notícias</h1>
 <p>Este site foi criado na disciplina de Construção de Páginas Web III do curso de tecnologia em Sistemas para Internet</p>
+<div class="contoner">
+    <h2>Alunos:</h2>
+    <p>Bruno Bento</p>
+    <p>Julia Arantes</p>
+    <p>Kaua Vinicios</p>
+</div>
